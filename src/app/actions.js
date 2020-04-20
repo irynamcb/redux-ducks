92bc58cbfd3c27a53cea0f2d88e2a08f58a412f7
irynamcb <iryna.mcbride@gmail.com>
2020-04-20 12:59:48 -0700

    Add createSlice

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,32 +1,32 @@
-const increment = createAction('INCREMENT')
-const decrement = createAction('DECREMENT')
+// const increment = createAction('INCREMENT')
+// const decrement = createAction('DECREMENT')
 
-// function increment() {
-//     return { type: INCREMENT }
-// }
+// const counter = createReducer(0, {
+//     [increment.type]: state => sgtate + 1,
+//     [decrement.type]: state => state - 1
+// })
 
-// function decrement() {
-//     return { type: DECREMENT }
-// }
+// const store = configureStore({
+//     reducer: counter
+// })
 
-const counter = createReducer(0, {
-    [increment.type]: state => sgtate + 1,
-    [decrement.type]: state => state - 1
+// document.getElementById('increment').addEventListener('click', () => {
+//     store.dispatch(increment())
+// })
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: 0,
+    reducers: {
+        increment: state => state + 1,
+        decrement: state => state - 1
+    }
 })
 
-// function counter(state = 0, action) {
-//     switch (action.type) {
-//         case increment.type:
-//             return state + 1
-//         case decrement.type:
-//             return state - 1
-//         default:
-//             return state
-//     }
-// }
-
-const store = Redux.createStore(counter)
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 
 document.getElementById('increment').addEventListener('click', () => {
-    store.dispatch(increment())
+    store.dispatch(counterSlice.actions.increment())
 })
